22664ff3ab585b4c8b6fa543d4ea762af988c904
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-11-01 21:09:14 +0900

    자릿수 더하기 (22.11.01)

---
+++ Algorithm/Lv0/자릿수 더하기(22.11.01).kts
@@ -0,0 +1,20 @@
+// 2022-11-01
+
+// 프로그래머스 Lv0 (자릿수 더하기)
+
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        var sum: Int = 0
+        var num: Int = n
+
+        while (num > 0) {
+            sum += num % 10
+            num = num / 10
+        }
+
+        answer = sum
+
+        return answer
+    }
+}
