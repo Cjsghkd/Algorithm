2219392ef3901de69579fed4d93ba550f6dfb742
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-11-04 14:12:24 +0900

    몫 구하기

---
+++ Algorithm/Lv_0/몫 구하기.kts
@@ -0,0 +1,18 @@
+// 2022-11-04
+
+// 프로그래머스 Lv0 (몫 구하기)
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+
+        answer = num1 / num2
+
+        return answer
+    }
+}
+
+// 가장 많은 답안
+// class Solution {
+//    fun solution(num1: Int, num2: Int) = num1 / num2
+//}
