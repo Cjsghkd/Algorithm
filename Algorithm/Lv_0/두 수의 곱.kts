4096770353f35f86d70a7dd4ef63cbebf8c51703
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-11-04 14:23:39 +0900

    두 수의 곲

--- Algorithm/Lv_0/두 수의 곱.kts
+++ Algorithm/Lv_0/두 수의 곱.kts
@@ -1,13 +1,12 @@
 // 2022-11-04
 
-// 프로그래머스 Lv0 (숫자 비교하기)
+// 프로그래머스 Lv0 (두 수의 곱)
 
 class Solution {
     fun solution(num1: Int, num2: Int): Int {
         var answer: Int = 0
 
-        if (num1 == num2) answer = 1
-        else answer = -1
+        answer = num1 * num2
 
         return answer
     }
@@ -15,5 +14,5 @@ class Solution {
 
 // 가장 많은 답안
 // class Solution {
-//    fun solution(num1: Int, num2: Int) = if(num1==num2) 1 else -1
+//    fun solution(num1: Int, num2: Int)= num1*num2
 //}
