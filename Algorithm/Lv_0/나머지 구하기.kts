110f5505508faca4e1f7a6d2db393937bcb55aa5
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-11-07 10:23:46 +0900

    나머지 구하기

---
+++ Algorithm/Lv_0/나머지 구하기.kts
@@ -0,0 +1,18 @@
+// 2022-11-07
+
+// 프로그래머스 Lv0 (나머지 구하기)
+
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int
+
+        answer = num1 % num2
+
+        return answer
+    }
+}
+
+// 가장 많은 답안
+// class Solution {
+//    fun solution(num1: Int, num2: Int) = num1 % num2
+//}
