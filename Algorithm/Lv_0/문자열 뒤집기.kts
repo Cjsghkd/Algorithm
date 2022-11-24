e6384921b522ddcbe75a9560f93b639014984bbf
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2022-11-24 17:22:32 +0900

    문자열 뒤집기

---
+++ Algorithm/Lv_0/문자열 뒤집기.kts
@@ -0,0 +1,13 @@
+// 2022-11-24
+
+// 프로그래머스 Lv0 (문자열 뒤집기)
+
+class Solution {
+    fun solution(my_string: String): String {
+        var answer: String = ""
+
+        answer = my_string.reversed()
+
+        return answer
+    }
+}
